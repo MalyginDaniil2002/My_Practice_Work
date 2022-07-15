b23cb330e136cec8b33891c3366d45baa267fd43
MalyginDaniil2002 <109207766+MalyginDaniil2002@users.noreply.github.com>
2022-07-15 17:56:49 +0300

    Delete Program.cs

--- Program.cs
+++
@@ -1,9 +0,0 @@
-using My_PracticeWork.Controllers;
-using My_PracticeWork.Models;
-using System.Text.RegularExpressions;
-var builder = WebApplication.CreateBuilder();
-var app = builder.Build();
-app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=My_Controller}/{action=app}");
-app.Run();
