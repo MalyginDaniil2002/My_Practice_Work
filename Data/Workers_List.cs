dc80634224f0081b21d8fbda8816afb11725a6d0
MalyginDaniil2002 <109207766+MalyginDaniil2002@users.noreply.github.com>
2022-07-17 09:13:05 +0300

    Add files via upload

---
+++ Data/Workers_List.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using My_Practice_Work.Models;
+namespace My_Practice_Work.Data
+{
+    public class Workers_List : DbContext
+    {
+        public DbSet<Worker> Workers { get; set; } = null!;
+        public Workers_List(DbContextOptions<Workers_List> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocald;Database=My_Practice;Trusted_Connection=True;");
+        }
+    }
+}
