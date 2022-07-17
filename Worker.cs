4ba7fef4c6a24197f083645d30a5a1efddd7a0b0
MalyginDaniil2002 <109207766+MalyginDaniil2002@users.noreply.github.com>
2022-07-17 08:48:02 +0300

    Delete Worker.cs

--- Worker.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace My_Practice_Work.Models
-{
-    public class Worker
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = "";
-        public int Age { get; set; }
-    }
-}
