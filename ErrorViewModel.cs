7118db62e026e6b132c424ec8d57db0b55b2289b
MalyginDaniil2002 <109207766+MalyginDaniil2002@users.noreply.github.com>
2022-07-17 08:29:44 +0300

    Delete ErrorViewModel.cs

--- ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace My_Practice_Work.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
