0af0c10fb3876f3516ddb097dc7f3ee2c0bca19a
MalyginDaniil2002 <109207766+MalyginDaniil2002@users.noreply.github.com>
2022-07-20 16:36:18 +0300

    Update Users_Work.cs

--- Data/Users_Work.cs
+++ Data/Users_Work.cs
@@ -4,14 +4,15 @@ namespace My_Practice_Work.Data
 {
     public class Users_Work : DbContext
     {
-        public DbSet<Person> Users { get; set; } = null!;
+        public DbSet<Person> Users { get; set; }
+        public DbSet<Worker> Workers { get; set; }
         public Users_Work(DbContextOptions<Users_Work> options) : base(options)
         {
             Database.EnsureCreated();
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=My_Practice_Work_Users;Trusted_Connection=True;");
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=My_Practice_Work;Trusted_Connection=True;");
         }
     }
 }
