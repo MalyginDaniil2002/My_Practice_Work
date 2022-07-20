25d5c1ffe7b63723410ce3928d99512883747f63
MalyginDaniil2002 <109207766+MalyginDaniil2002@users.noreply.github.com>
2022-07-20 16:35:33 +0300

    Add files via upload

---
+++ Database/dbo.Users.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+    [Id]          INT            NOT NULL PRIMARY KEY,
+    [Name]        NVARCHAR (MAX) NULL,
+    [Surname]     NVARCHAR (MAX) NULL,
+    [Middle_Name] NVARCHAR (MAX) NULL,
+    [Birth_Year]  INT            NULL,
+    [Birth_Month] INT            NULL,
+    [Birth_Day]   INT            NULL,
+);
